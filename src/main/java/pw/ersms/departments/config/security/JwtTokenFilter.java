d2273d62c2228d48aaea0985c9dd0b713d48dd0b
AnejVovcak <anej.vovcak@gmail.com>
2023-06-09 15:38:51 +0200

    auth implementation

---
+++ src/main/java/pw/ersms/departments/config/security/JwtTokenFilter.java
@@ -0,0 +1,97 @@
+package pw.ersms.departments.config.security;
+
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenUtil jwtUtil;
+
+    private boolean hasAuthorizationBearer(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (ObjectUtils.isEmpty(header) || !header.startsWith("Bearer")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private String getAccessToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        String token = header.split(" ")[1].trim();
+        return token;
+    }
+
+    private void setAuthenticationContext(String token, HttpServletRequest request) {
+        UserDetails userDetails = getUserDetails(token);
+
+        UsernamePasswordAuthenticationToken
+                authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        System.out.println("authentication: " + authentication);
+
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private UserDetails getUserDetails(String token) {
+
+        Claims claims = jwtUtil.parseClaims(token);
+        String subject = (String) claims.get(Claims.SUBJECT);
+        String role = (String) claims.get("roles");
+        System.out.println("roles: " + role);
+
+        System.out.println("roleNames: " + role);
+
+        ArrayList<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role));
+        String[] jwtSubject = subject.split(",");
+
+        System.out.println("email: " + jwtSubject[1]);
+
+        //userDetails.setId(Integer.parseInt(jwtSubject[0]));
+        //userDetails.setEmail(jwtSubject[1]);
+
+        UserDetails userDetails = new User(jwtSubject[1], "", authorities);
+        System.out.println("userDetails: " + userDetails);
+        return userDetails;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (!hasAuthorizationBearer(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = getAccessToken(request);
+
+        if (!jwtUtil.validateAccessToken(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        setAuthenticationContext(token, request);
+        filterChain.doFilter(request, response);
+    }
+}
